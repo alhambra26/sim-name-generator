5218d09dcd9d701d6dbdac9809605c32265c60e1
alhambra26 <machioadrian@gmail.com>
2026-08-12 18:07:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 function generarNombre() {
-    const nombreAleatorio = nombres[
-        Math.floor(Math.random() * nombres.length)
-    ];
 
-    alert(nombreAleatorio.nombre);
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    const genero = document.querySelector('input[name="genero"]:checked').value;
+
+    let nombresFiltrados = nombres;
+
+    if (genero !== "ambos") {
+        nombresFiltrados = nombres.filter(
+            persona => persona.genero === genero
+        );
+    }
+
+    if (tipo === "nombre") {
+
+        const elegido = nombresFiltrados[
+            Math.floor(Math.random() * nombresFiltrados.length)
+        ];
+
+        document.getElementById("resultado").textContent = elegido.nombre;
+
+    } else if (tipo === "apellido") {
+
+        const elegido = apellidos[
+            Math.floor(Math.random() * apellidos.length)
+        ];
+
+        document.getElementById("resultado").textContent = elegido.apellido;
+
+    } else if (tipo === "completo") {
+
+        const nombreElegido = nombresFiltrados[
+            Math.floor(Math.random() * nombresFiltrados.length)
+        ];
+
+        const apellidoElegido = apellidos[
+            Math.floor(Math.random() * apellidos.length)
+        ];
+
+        document.getElementById("resultado").textContent =
+            nombreElegido.nombre + " " + apellidoElegido.apellido;
+    }
 }
