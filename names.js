b96ca92791c11f85f53850cfdbf81226b57cf299
alhambra26 <machioadrian@gmail.com>
2026-08-12 17:59:14 +0200

    Update names.js

--- names.js
+++ names.js
@@ -1,1 +1,56 @@
+const nombres = [
+    {
+        nombre: "Freya",
+        genero: "femenino",
+        cantera: "mitologia",
+        subcategoria: "nordica"
+    },
+    {
+        nombre: "Astrid",
+        genero: "femenino",
+        cantera: "historia",
+        subcategoria: "nordica"
+    },
+    {
+        nombre: "Diana",
+        genero: "femenino",
+        cantera: "mitologia",
+        subcategoria: "romana"
+    },
+    {
+        nombre: "Orion",
+        genero: "masculino",
+        cantera: "mitologia",
+        subcategoria: "griega"
+    },
+    {
+        nombre: "Leif",
+        genero: "masculino",
+        cantera: "historia",
+        subcategoria: "nordica"
+    },
+    {
+        nombre: "Arthur",
+        genero: "masculino",
+        cantera: "literatura",
+        subcategoria: "arturica"
+    }
+];
 
+const apellidos = [
+    {
+        apellido: "Blackwood",
+        cantera: "literatura",
+        subcategoria: "gotica"
+    },
+    {
+        apellido: "Montgomery",
+        cantera: "historia",
+        subcategoria: "nobleza"
+    },
+    {
+        apellido: "Whitmore",
+        cantera: "historia",
+        subcategoria: "inglesa"
+    }
+];
